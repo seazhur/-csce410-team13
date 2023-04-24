86655dba3a06a56c637cb5f3aec4dda44083dd2c
Adidev Mohapatra <adidevmohapatra@client-10-228-58-200.tamulink.tamu.edu>
2023-04-24 13:47:22 -0500

    test

--- login/login.php
+++ login/login.php
@@ -3,6 +3,7 @@
 
 <head>
   <link rel="stylesheet" href="styles.css">
+  
 </head>
 
 <body>
--- profile/read.php
+++ profile/read.php
@@ -6,12 +6,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My Profile</title>
   <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="profile.css" importance="high">
   <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 </head>
 <body>
 
   <!--Navigation bar-->
   <div id="nav-placeholder"></div>
+    <h2>My Profile</h2>
+      <?php
+      //connect to the database
+      $conn = new mysqli("localhost", "Cesar", "DX8317oZ]XFs0mMo", "trip2gether");
+      if (!$conn) { die("Connection failed: " . $conn->connect_error); }
+    
+    
+     ?>
 
 
 

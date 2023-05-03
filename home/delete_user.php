740d1f6ec1a92b7b0ca9240d686ee4c4f1a61cd5
Adidev Mohapatra <adidevmohapatra@client-10-229-201-12.tamulink.tamu.edu>
2023-05-03 15:55:28 -0500

    delete user

--- home/delete_user.php
+++ home/delete_user.php
@@ -30,9 +30,9 @@ if (mysqli_num_rows($result) === 0) {
 // Delete the user from the database
 $query = "DELETE FROM users WHERE username = '$username'";
 if (mysqli_query($conn, $query)) {
-    echo "User '$username' deleted successfully. <a href='authuser.php'>Go back to profile</a>";
+    echo "User '$username' deleted successfully. <a href='authprofile.php'>Go back to profile</a>";
 } else {
-    echo "Error: " . mysqli_error($conn) . "<a href='authuser.php'>Go back to profile</a>";
+    echo "Error: " . mysqli_error($conn) . "<a href='authprofile.php'>Go back to profile</a>";
 }
 
 mysqli_close($conn);
